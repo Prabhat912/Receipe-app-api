55b63b1df9f817a523ee1da0121c64d1bad01ff6
Prabhat Nagargade <nagargadeprabhat@gmail.com>
2023-08-29 23:29:58 +0530

    Apply Validation for email at model and also added the test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,3 +35,10 @@ class ModelTests(TestCase):
         for email, expectedEmail in sampleEmails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expectedEmail)
+    
+    def test_new_user_without_email_raise_error(self):
+        """
+        Test that creating a user without an email will raise a Value Error
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
